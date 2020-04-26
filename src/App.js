eb0e6ec1b208e300c86deb0cd84766c16eac2d95
susanavbarbosa <susanavbarbosa@users.noreply.github.com>
2020-04-26 21:51:53 +0100

    Footer and card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Weather />
+      <footer>
+        Coded by{" "}
+        <a href="https://pt.linkedin.com/in/susanavbarbosa" target="_blank">
+          SUsana Barbosa{" "}
+        </a>{" "}
+        and
+        <a
+          href="https://github.com/susanavbarbosa/SheCodesWeatherBySU"
+          target="_blank"
+        >
+          {" "}
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
---
+++ src/Weather.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./App.css";
+import "./Weather.css";
+
+export default function Weather() {
+  return <div className="card"> Weather</div>;
+}
